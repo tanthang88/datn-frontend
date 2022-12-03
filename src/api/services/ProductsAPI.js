7a641d0d137b6c6ab804d3155b71458f35bd1bd1
trungtv <trungtv@kozo-japan.com>
2022-12-04 01:08:48 +0700

    trungtv/detail-product update code

--- src/api/services/ProductsAPI.js
+++ src/api/services/ProductsAPI.js
@@ -25,6 +25,10 @@ export const fetchCommentsProduct = async (payload) => {
 }
 
 export const fetchProductsRelated = async (payload) => {
-  const { data } = await publicRequest.get(`product/categories/${payload}`)
+  const { data } = await publicRequest.get(`product/${payload}/related`)
+  return data
+}
+export const createComment = async (id, payload) => {
+  const { data } = await publicRequest.post(`product/${id}/comment`, payload)
   return data
 }
