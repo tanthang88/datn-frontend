ff4eb77b4d7e1c17d19cebddbe551989ceb3f944
trungtv <trungtv@kozo-japan.com>
2022-12-26 00:52:29 +0700

    trungtv/detail-product update code product

--- src/pages/Product/components/Info.jsx
+++ src/pages/Product/components/Info.jsx
@@ -50,25 +50,30 @@ export default function Info({ configurations }) {
   ]
 
   return (
-    <table className='border-collapse border border-slate-400 w-full'>
-      <thead>
-        <tr>
-          <th className='border border-slate-300 bg-slate-200 p-2 w-[150px]'>
-            Cấu hình
-          </th>
-          <th className='border border-slate-300 bg-slate-200 p-2'>
-            Thông số chi tiết
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {listConfigs.map((item) => (
-          <tr key={item.id}>
-            <td className='border border-slate-300 p-2'>{item.title}</td>
-            <td className='border border-slate-300 p-2'>{item.value}</td>
+    <div className='w-full bg-white p-5 mb-5'>
+      <h2 className='text-xl font-normal leading-normal mt-0 mb-2'>
+        Thông số kỹ thuật
+      </h2>
+      <table className='border-collapse border border-slate-400 w-full'>
+        <thead>
+          <tr>
+            <th className='border border-slate-300 bg-slate-200 p-2 w-[150px]'>
+              Cấu hình
+            </th>
+            <th className='border border-slate-300 bg-slate-200 p-2'>
+              Thông số chi tiết
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {listConfigs.map((item) => (
+            <tr key={item.id}>
+              <td className='border border-slate-300 p-2'>{item.title}</td>
+              <td className='border border-slate-300 p-2'>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
